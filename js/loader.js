880f486f6a3f361d8f617f3829aa05970d6b3707
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-07-07 20:52:24 +0530

    Add import fn

--- js/loader.js
+++ js/loader.js
@@ -1,24 +1,32 @@
 class WasmLoader {
-  constructor() {}
+  constructor() {
+    this._imports = {
+      "env": {
+        abort() {
+          throw new Error('Abort called from wasm file');
+        }
+      }
+    };
+  }
 
-  async wasm(path) {
+  async wasm(path, imports = this._imports) {
     console.log(`Fethcing ${path}`);
 
     if(!WebAssembly.instantiateStreaming) {
-      return this.wasmFallback(path);
+      return this.wasmFallback(path, imports);
     }
 
-    const { instance } = await WebAssembly.instantiateStreaming(fetch(path));
+    const { instance } = await WebAssembly.instantiateStreaming(fetch(path), imports);
 
     return instance?.exports;
   }
 
-  wasmFallback(path) {
+  wasmFallback(path, imports) {
     console.log(`Using fallback ${path}`);
 
     const response = await fetch(path);
     const bytes = await response?.arrayBuffer();
-    const { instance } = WebAssembly.instantiate(bytes);
+    const { instance } = WebAssembly.instantiate(bytes, imports);
 
     return instance?.exports;
   }
